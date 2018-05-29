eb45e0f04ea88e8cd2df5075580e57646eb0d215
jpca999 <jpca999@gmail.com>
2018-05-28 18:00:30 -0700

    but it doesnt fire on first time

--- app.js
+++ app.js
@@ -15,18 +15,56 @@ const counter = (state = 0, action) => {
 const { createStore } = Redux;
 const store = createStore(counter);
 
-console.log(store.getState());
-// store.dispatch({type: 'INCREMENT'});
+store.dispatch ({ type: 'INCREMENT' });
 
-const render = () => {
+console.log(' state.getState =', store.getState());
+
+// anytime an action is dispatched, this store.subscribe will be called.
+store.subscribe (() => {
+    console.log(' calling the store.subscribe  method');
     document.body.innerText = store.getState();
-};
-
-store.subscribe(render);
-  render();
-
-console.log(store.getState());
+  });
 
 document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
+    store.dispatch({ type: 'INCREMENT'});
 });
+
+// console.log(store.getState());
+// store.dispatch({type: 'INCREMENT'});
+
+// const createStore = (reducer) => {
+//   let state;
+//   let listeners = [];
+//
+//   const getState = () => state;
+//
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(listener => listener());
+//   };
+//
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
+//     return () => {
+//       listeners = listeners.filter( l=> l !== listener );
+//     };
+//   };
+//
+//   dispatch ({   });
+//   return {getState, dispatch, subscribe };
+//
+// }
+//
+//
+// const render = () => {
+//     document.body.innerText = store.getState();
+// };
+//
+// store.subscribe(render);
+//   render();
+//
+// console.log(store.getState());
+//
+// document.addEventListener('click', () => {
+//   store.dispatch({ type: 'INCREMENT' });
+// });
